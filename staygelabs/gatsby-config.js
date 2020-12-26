4b29913625905f49b1e27530fc3bd406c468c9f6
Anastasia Bourlas <bourlas.3@osu.edu>
2020-12-25 19:26:25 -0500

    initial app

---
+++ staygelabs/gatsby-config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "gatsby-starter-default",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#000000",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/images/gatsby-icon.png", // This path is relative to the root of the site.
+      },
+    },
+    "gatsby-plugin-sass",
+  ],
+};
